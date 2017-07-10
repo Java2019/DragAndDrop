65d4ac57eadc8fde1715110846cf1ff6b8673732
java2019 <java_2019@mail.ru>
2017-07-10 16:53:21 +0300

    Initial commit

---
+++ app/src/main/java/com/simples/res/dragndrop/DragNDropActivity.java
@@ -0,0 +1,16 @@
+package com.simples.res.dragndrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class DragNDropActivity extends AppCompatActivity {
+
+    private ImageView ima;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag_ndrop);
+    }
+}
